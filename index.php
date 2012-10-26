c3a4cf06ca80587256e291283ad112343fd48225
Brandon <hollen949b@gmail.com>
2012-10-25 21:17:02 -0700

    Pushing first file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$localcheck = array('localhost', '127.0.0.1');
+
+if (in_array($_SERVER['HTTP_HOST'], $localcheck)) {
+    $yii = dirname(__FILE__) . '/../../Yii/framework/yii.php';
+} else {
+    $yii = dirname(__FILE__).'/framework/yii.php';
+}
+
+$config = dirname(__FILE__) . '/protected/config/front.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+
+require_once($yii);
+Yii::createWebApplication($config)->runEnd('front');
